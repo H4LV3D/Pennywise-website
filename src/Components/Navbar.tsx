29e570c870ffaae0986f69535e11093f26852090
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-01 22:03:42 +0100

    Refactor Navbar component

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -19,34 +19,17 @@ function Header({ nav }: Props) {
     { text: "Pricing", link: "/contact" },
     { text: "Features", link: "/contact" },
   ];
-  const [hasShadow, setHasShadow] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const shouldHaveShadow = window.scrollY > 800;
-      setHasShadow(shouldHaveShadow);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const headerClasses = `font-raleway flex items-center justify-between py-5 md:py-3 ${
-    nav
-      ? `lg:border-b border-neutral-50 dark:border-[#191919]`
-      : "md:border-b border-neutral-100 dark:border-[#191919] sm:border-none"
-  } ${hasShadow ? "shadow-sm border-gray-200" : ""}`;
 
   return (
     <div className="fixed top-0 left-0 w-full bg-[#fbfbfb] dark:bg-[#000B17] bg-opacity-75 z-20">
       <div className="px-6 lg:px-0 md:container mx-auto xl:w-[1250px]">
-        <div className={`${headerClasses}`}>
+        <div
+          className={`flex items-center justify-between py-5 md:py-3   shadow-sm `}
+        >
           <div className="flex items-center space-x-6 text-black dark:text-neutral-400">
             <div className="hidden lg:block group relative dark:border-gray-600 dark:text-neutral-300 cursor-pointer">
               <BrandLogo />
-              <p className="opacity-0 bg-black dark:bg-neutral-900 dark:text-neutral-400 text-white text-center text-xs font-raleway rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-4 mt-4 px-4 pointer-events-none">
+              <p className="opacity-0 bg-black dark:bg-neutral-900 dark:text-neutral-400 text-white text-center text-xs rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-4 mt-4 px-4 pointer-events-none">
                 Home
               </p>
             </div>
