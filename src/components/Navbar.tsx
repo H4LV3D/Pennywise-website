6d4e6a1fe9c2c9dcb06a774ca0845f056af70f7a
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-02-02 07:05:45 +0100

    Refactor Navbar component and update ToggleButton import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { usePathname } from "next/navigation";
 import BrandLogo from "./Logo";
 import Link from "next/link";
-import ColorModeToggle from "./ModeToggle";
+import ToggleButton from "./ToggleButton";
 
 type Props = {};
 
@@ -17,12 +17,12 @@ function Header({}: Props) {
   ];
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-[#fbfbfb] dark:bg-[#000B17] bg-opacity-75 z-20">
+    <div className="fixed top-0 left-0 w-full bg-[#fbfbfb] dark:bg-[#000B17] bg-opacity-75 z-40">
       <div className="px-6 lg:px-0 mx-auto xl:w-[1250px]">
         <div
           className={`flex items-center justify-between py-5 md:py-3   shadow-sm `}
         >
-          <div className="flex items-center space-x-6 text-black dark:text-neutral-400">
+          <div className="flex items-center lg:space-x-6 text-black dark:text-neutral-400">
             <div className="hidden lg:block group relative dark:border-gray-600 dark:text-neutral-300 cursor-pointer">
               <BrandLogo />
               <p className="opacity-0 bg-black dark:bg-neutral-900 dark:text-neutral-400 text-white text-center text-xs rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-4 mt-4 px-4 pointer-events-none">
@@ -57,7 +57,7 @@ function Header({}: Props) {
           </div>
 
           <div className="flex items-center space-x-3 ">
-            <ColorModeToggle />
+            <ToggleButton />
             <button
               data-aos="fade-down"
               data-aos-delay={1200}
