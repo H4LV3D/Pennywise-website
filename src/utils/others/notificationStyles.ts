81e5802f3b6ab448effe60e2694c790777ad1c69
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-01 10:48:26 +0100

    Add notification styles to utils/others/notificationStyles.ts

---
+++ src/utils/others/notificationStyles.ts
@@ -0,0 +1,24 @@
+export const notificationStyles = {
+  backgroundColor: "#023047",
+  color: "#FBFBFB",
+  borderColor: "rgba(255, 255, 255, 0.08)",
+  borderWidth: "1.5px",
+};
+
+export const notificationStyles2 = () => ({
+  title: {
+    color: "#FBFBFB",
+    fontSize: "1rem",
+    fontWeight: "bold",
+  },
+  body: {
+    color: "#FBFBFB",
+    fontSize: "0.9rem",
+  },
+  close: {
+    color: "#FBFBFB",
+  },
+  icon: {
+    color: "#FBFBFB",
+  },
+});
