ed0a937cd0e9e0e219a6b56546f8d1ce2127a919
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-01 09:57:04 +0100

    Add WaitListModal component

---
+++ src/components/modals/WaitListModal.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { useState } from "react";
+import WaitlistForm from "../forms/Waitlist";
+
+type Props = {};
+
+const WaitListModal = ({}: Props) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  return <>{isModalOpen && <WaitlistForm closeModal={setIsModalOpen} />}</>;
+};
+
+export default WaitListModal;
