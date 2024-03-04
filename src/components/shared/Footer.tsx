30dcd810a98ac66489381e8a93ba85591e33eef0
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-04 08:13:45 +0100

    Update Footer component styles

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -61,14 +61,14 @@ function Footer({}: Props) {
   return (
     <>
       <div className="w-full font-raleway bg-neutral-100 dark:bg-[#000B17] dark:text-neutral-400 py-20">
-        <div className=" md:container mx-auto xl:w-[950px]">
+        <div className=" md:container mx-auto lg:w-[950px]">
           <div className="md:flex flex-row lg:grid sm:grid-cols-2 mb-8">
             <div className="w-full sm:w-1/3 lg:w-full mb-8 md:mb-4 ">
               <BrandLogo />
 
               <p
                 data-aos="fade-right"
-                className="text-sm font-urbanist font-[400] sm:text-base lg:text-base !leading-6 md:!leading-7 mt-4 text-justify sm:hidden lg:block lg:w-2/3 xl:w-3/4"
+                className="text-[.9375rem]  font-[400] sm:text-base lg:text-base !leading-6 md:!leading-7 mt-4 text-justify sm:hidden lg:block lg:w-2/3 xl:w-3/4"
               >
                 We help you make the best financial decisions by providing you
                 with the right information and tools. We are your one stop shop
@@ -134,7 +134,7 @@ function Footer({}: Props) {
 
           <hr className="mb-4 dark:border-neutral-700" />
 
-          <div className="md:grid grid-cols-1 sm:grid-cols-2 items-center py-2 mt-4">
+          <div className="flex justify-between md:grid grid-cols-1 sm:grid-cols-2 items-center py-2 mt-4">
             <div className="flex items-center font-raleway font-normal sm:text-base text-sm text-neutral-600 dark:text-neutral-400 space-x-4 mb-3 md:mb-0">
               <span className=" ">All rights reserved.</span>
               <span className="font-number ">© {date} </span>
@@ -149,7 +149,7 @@ function Footer({}: Props) {
                   data-aos-delay={index * 150}
                   className=" rounded-md hover:border-black dark:hover:border-white"
                 >
-                  <div className="group relative font-raleway font-normal cursor-pointer dark:text-white text-black p-2">
+                  <div className="group relative font-raleway font-normal cursor-pointer dark:text-white text-black">
                     <i className={`${item.icon} fa-xl fa-fw`}></i>
                     <div className="opacity-0 bg-black text-white text-center text-xs font-raleway rounded-lg py-2 absolute z-40 group-hover:opacity-100 top-full -left-1/2 px-4 pointer-events-none">
                       {item.title}
