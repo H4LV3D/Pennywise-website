1435676fbdfb53ac3343a724b670dadc8e86361d
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-18 09:37:48 +0100

    Update max width and container styles in PageLayout component

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -7,7 +7,9 @@ interface Props {
 
 const MaxWidthProvider: React.FC<Props> = ({ className, children }) => {
   return (
-    <div className={`${className} max-w-[84rem] mx-auto px-6 md:px-8`}>
+    <div
+      className={`${className} max-w-[84rem] md:container xl:w-[1250px] mx-auto px-6 sm:px-8 md:px-0 `}
+    >
       {children}
     </div>
   );
