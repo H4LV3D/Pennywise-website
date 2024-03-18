b92e233b05e11b40c41b346b40685312d2f353e9
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-18 09:38:08 +0100

    Refactor contact page layout and styling

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -16,15 +16,17 @@ export default function Contact() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={` min-h-screen bg-[#fbfbfb] dark:bg-[#000B17] py-20 `}>
+      <div
+        className={` min-h-screen bg-[#fbfbfb] dark:bg-[#000B17] py-8 md:py-20 `}
+      >
         <PageLayout>
           <Navbar />
-          <div className="grid grid-cols-1 md:grid-cols-2 md:px-6 gap-y-8 min-h-[75vh] py-20 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-y-8 min-h-[75vh] py-20 place-items-center ">
             <div className="">
-              <h5 className="text-xl text-brand-blue dark:text-brand-white">
+              <h5 className="text-xl mb-2 text-brand-blue dark:text-brand-white">
                 Contact Us
               </h5>
-              <h1 className="text-6xl font-bold text-brand-blue dark:text-brand-white">
+              <h1 className="text-5xl lg:text-6xl font-bold text-brand-blue dark:text-brand-white">
                 Get In Touch
               </h1>
               <p className="mt-3 text-[1rem] text-[#6464646] dark:text-neutral-400 max-w-[30rem] tracking-wide  ">
@@ -32,18 +34,11 @@ export default function Contact() {
                 have. We are currently available to take any message at the
                 moment.
               </p>
-              <div className="mt-8">
-                <a
-                  href="mailto:hello@iampennywise.com"
-                  data-aos="fade-right"
-                  className="text-sm font-[400] sm:text-base lg:text-base hover:text-brand-white flex items-center text-brand-green space-x-2 "
-                >
-                  <i className="fas fa-envelope fa-fw fa-md"></i>
-                  <span>hello@iampennywise.com</span>
-                </a>
-              </div>
+              <div className="mt-8"></div>
             </div>
-            <ContactForm />
+            <div className=" w-full ">
+              <ContactForm />
+            </div>
           </div>
           <Footer />
         </PageLayout>
