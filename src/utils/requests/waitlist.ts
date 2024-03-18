662c98b71c26c89ab00bb979a38a9e88ec3a059d
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-03-18 09:38:30 +0100

    Refactor API calls in waitlist.ts

--- src/utils/requests/waitlist.ts
+++ src/utils/requests/waitlist.ts
@@ -5,19 +5,11 @@ export interface Waitlist {
 }
 
 export const addToWaitList = async (data: Waitlist) => {
-  const res = await appAxios.post("/email", data, {
-    headers: {
-      api_key: process.env.NEXT_PUBLIC_API_KEY as string,
-    },
-  });
+  const res = await appAxios.post("/email", data);
   return res;
 };
 
 export const getWaitlistNumber = async () => {
-  const res = await appAxios.get("/users", {
-    headers: {
-      api_key: process.env.NEXT_PUBLIC_API_KEY as string,
-    },
-  });
+  const res = await appAxios.get("/users");
   return res;
 };
