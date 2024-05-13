10d26d1faa5b211f18fcb4b2f84b2f69960ff609
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-05-13 02:00:25 +0100

    refactor: Update waitlist page layout and styles

--- src/pages/waitlist/index.tsx
+++ src/pages/waitlist/index.tsx
@@ -1,12 +1,10 @@
-"use client";
 import PageLayout from "@/layout/PageLayout";
-import Navbar from "@/components/shared/Navbar";
-import Footer from "@/components/shared/Footer";
 import { Table } from "@mantine/core";
 import Head from "next/head";
 import { useQuery } from "@tanstack/react-query";
 import { getWaitlist } from "@/utils/requests/waitlist";
 import { WaitlistUser } from "@/typings/waitlist";
+import { showNotification } from "@mantine/notifications";
 
 export default function Contact() {
   const { data, isLoading, isError, error } = useQuery({
@@ -20,24 +18,39 @@ export default function Contact() {
     refetchOnWindowFocus: true,
   });
 
+  if (isError) {
+    showNotification({
+      title: "Error fetching waitlist.",
+      message: error.message,
+    });
+  }
+
   const ths = (
     <tr className="!text-brand-white font-[400] ">
       <th className="!text-brand-white font-[400]">No.</th>
-      <th className="!text-brand-white font-[400]">First Name</th>
-      <th className="!text-brand-white font-[400]">Last Name</th>
+      <th className="!text-brand-white font-[400] hidden sm:table-cell ">
+        First Name
+      </th>
+      <th className="!text-brand-white font-[400] hidden sm:table-cell">
+        Last Name
+      </th>
       <th className="!text-brand-white font-[400]">Email</th>
     </tr>
   );
 
   const rows = data?.map((element, index) => (
     <tr
-      className="text-brand-white hover:text-brand-blue !border-neutral-500 "
+      className="text-neutral-300 hover:font-[500] hover:text-brand-blue  "
       key={element?.firstName}
     >
-      <td>{index + 1}</td>
-      <td>{element?.firstName}</td>
-      <td>{element?.lastName}</td>
-      <td>{element?.email}</td>
+      <td className="!border-neutral-500">{index + 1}</td>
+      <td className="!border-neutral-500 hidden sm:table-cell ">
+        {element?.firstName}
+      </td>
+      <td className="!border-neutral-500 hidden sm:table-cell ">
+        {element?.lastName}
+      </td>
+      <td className="!border-neutral-500">{element?.email}</td>
     </tr>
   ));
 
@@ -51,42 +64,46 @@ export default function Contact() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={` min-h-screen bg-[#fbfbfb] dark:bg-[#000B17] py-16 md:py-20 `}
-      >
-        <PageLayout>
-          <Navbar />
-          <div className=" min-h-[75vh] py-8 md:py-8 ">
-            <div className="">
-              <h5 className="text-xl font-[500] uppercase tracking-wide text-brand-blue dark:text-brand-white">
-                Wait list
-              </h5>
-              <h1 className="text-5xl lg:text-6xl font-bold text-brand-blue dark:text-brand-white">
-                Congratulatons!
-              </h1>
-              <p className="mt-3 text-[1rem] text-[#6464646] dark:text-neutral-400 max-w-[30rem] tracking-wide  ">
-                You are now on the wait list. We will reach out to you as soon
-                as possible. Thank you for your patience.
-              </p>
-            </div>
-            <div className="my-8 border-b text-brand-white ">
-              <Table
-                className="hover:bg-opacity-20 "
-                highlightOnHover
-                captionSide="bottom"
-                verticalSpacing={"md"}
-                horizontalSpacing={"md"}
-                fontSize={"md"}
-                withBorder={false}
-              >
-                <thead>{ths}</thead>
-                <tbody>{rows}</tbody>
-              </Table>
-            </div>
+      <PageLayout>
+        <div className=" min-h-[75vh]  ">
+          <div className="pt-12 ">
+            <h5 className="text-base sm:text-lg font-[500] uppercase tracking-wide text-brand-blue dark:text-neutral-200">
+              Wait list
+            </h5>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl mt-0.5 font-bold text-brand-blue dark:text-brand-white">
+              Congratulatons!
+            </h1>
+            <p className="mt-3 text-[1rem] text-[#646464] dark:text-neutral-400 max-w-[30rem] tracking-wide  ">
+              You are now on the wait list. We will reach out to you as soon as
+              possible. Thank you for your patience.
+            </p>
           </div>
-          <Footer />
-        </PageLayout>
-      </div>
+          <div
+            className={`my-10 ${!isLoading && "border-b"} text-brand-white `}
+          >
+            <Table
+              className="hover:bg-opacity-20 border-neutral-500 dark:border-neutral-500"
+              highlightOnHover
+              captionSide="bottom"
+              verticalSpacing={"md"}
+              horizontalSpacing={"md"}
+              fontSize={"md"}
+              withBorder={false}
+            >
+              <thead>{ths}</thead>
+              <tbody>{rows}</tbody>
+            </Table>
+            {isLoading && (
+              <div className="min-h-[30vh] w-full flex items-center justify-center ">
+                <div className=" text-center ">
+                  <i className="fas fa-spinner fa-spin fa-2x text-brand-blue dark:text-brand-white"></i>
+                  <p className=" text-brand-white text-lg ">Loading</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </PageLayout>
     </>
   );
 }
