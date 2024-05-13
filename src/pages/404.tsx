57a9a1149f9b8190b654337e922be4fea1b9aa52
Moyinoluwa <64863887+H4LV3D@users.noreply.github.com>
2024-05-13 01:59:37 +0100

    refactor: Remove unused imports and update 404 page layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,4 @@
-"use client";
 import PageLayout from "@/layout/PageLayout";
-import Navbar from "@/components/shared/Navbar";
-import Footer from "@/components/shared/Footer";
-import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -11,33 +7,24 @@ export default function Error() {
     <>
       <Head>
         <title>Pennywise | Not Found </title>
-        <meta
-          name="description"
-          content="Pennywise is a financial accounting app that helps you make the best financial decisions by providing you with the right information and tools."
-        />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={` min-h-screen bg-[#fbfbfb] dark:bg-[#000B17] pt-20 `}>
-        <PageLayout>
-          <Navbar />
-          <div className="flex flex-col items-center justify-center min-h-[75vh] py-2">
-            <h1 className="text-6xl font-bold text-center text-brand-blue dark:text-brand-white">
-              Looking for Pennies ? Oops!
-            </h1>
-            <p className="mt-3 text-[1.25rem] text-center text-[#6464646] dark:text-neutral-400 max-w-[30rem] ">
-              The page you are looking for does not exist. Please check the URL
-              and try again.
-            </p>
-            <Link href="/">
-              <button className="dark:bg-brand-blue text-brand-white h-[3rem] px-8 rounded-[0.5rem] font-[500] flex items-center space-x-2 mt-3 ">
-                <span>Go Home</span>
-                <i className="fas fa-chevron-right fa-fw fa-md text-white "></i>
-              </button>
-            </Link>
-          </div>
-          <Footer />
-        </PageLayout>
-      </div>
+      <PageLayout>
+        <div className="flex flex-col items-center justify-center min-h-[75vh] py-2">
+          <h1 className="text-6xl font-bold text-center text-brand-blue dark:text-brand-white">
+            Looking for Pennies ? Oops!
+          </h1>
+          <p className="mt-3 text-[1.25rem] text-center text-[#6464646] dark:text-neutral-400 max-w-[30rem] ">
+            The page you are looking for does not exist. Please check the URL
+            and try again.
+          </p>
+          <Link href="/">
+            <button className="dark:bg-brand-blue text-brand-white h-[3rem] px-8 rounded-[0.5rem] font-[500] flex items-center space-x-2 mt-3 ">
+              <span>Go Home</span>
+              <i className="fas fa-chevron-right fa-fw fa-md text-white "></i>
+            </button>
+          </Link>
+        </div>
+      </PageLayout>
     </>
   );
 }
